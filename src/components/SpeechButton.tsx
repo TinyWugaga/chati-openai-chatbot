e2224c243a87e2e58052b0e21f8efb5b60f96d8a
Tinywugaga <tiny98712@gmail.com>
2023-02-03 02:47:53 +0800

    modify SpeechButton

--- src/components/SpeechButton.tsx
+++ src/components/SpeechButton.tsx
@@ -1,45 +1,11 @@
-import styled from "styled-components";
+import styled, { DefaultTheme, keyframes, css } from "styled-components";
 
 import { MicIcon } from "@/components/Icons";
 import {
   backgroundHexColorDarken,
   backgroundHexColorLighten,
+  boxShadowBorder,
 } from "@/assets/styles/mixin";
-
-const Button = styled.button<{ active?: boolean }>`
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-
-  padding: 0.5rem;
-  font-size: 2rem;
-
-  cursor: pointer;
-  background: ${({ theme }) =>
-    backgroundHexColorDarken(
-      theme.colors.primary.main,
-      theme.colors.primary.dark
-    )};
-
-  color: inherit;
-  border: none;
-  border-radius: 1.5rem;
-  text-align: center;
-
-  &:hover:not([disabled]) {
-    background: ${({ theme }) =>
-      backgroundHexColorLighten(
-        theme.colors.primary.main + "80",
-        theme.colors.white.main
-      )};
-    color: ${({ theme }) => theme.colors.primary.dark};
-  }
-
-  &[disabled] {
-    cursor: auto;
-    color: ${({ theme }) => theme.colors.primary.dark + "80"};
-  }
-`;
 
 export default function SpeechButton({
   onClick,
@@ -56,3 +22,82 @@ export default function SpeechButton({
     </Button>
   );
 }
+
+const ButtonActiveKeyframe = (
+  theme: DefaultTheme,
+  backgroundColor: string
+) => keyframes`
+  0% {
+    background: ${backgroundColor};
+    color: inherit;
+  }
+
+  50% {
+    background: ${backgroundHexColorLighten(
+      theme.colors.white.main + "80",
+      theme.colors.primary.main
+    )};
+    color: ${theme.colors.black.main + "60"};
+  }
+
+  99% {
+    background: ${backgroundColor};
+    color: inherit;
+  }
+`;
+
+const ButtonActiveAnimation = ({
+  theme,
+  backgroundColor,
+}: {
+  theme: DefaultTheme;
+  backgroundColor: string;
+}) => css`
+  animation: 1.2s cubic-bezier(0.5, 0.4, 0.99, 0.88) 0s infinite normal
+    backwards running ${ButtonActiveKeyframe(theme, backgroundColor)};
+  transform-box: fill-box;
+  transform-origin: 50% 50%;
+`;
+
+const Button = styled.button.attrs(({ theme }) => ({
+  backgroundColor: backgroundHexColorDarken(
+    theme.colors.primary.main,
+    theme.colors.primary.dark
+  ),
+}))<{ active?: boolean }>`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 0.5rem;
+  font-size: 2rem;
+
+  cursor: pointer;
+  background: ${({ backgroundColor }) => backgroundColor};
+
+  color: inherit;
+  border: none;
+  border-radius: 1.5rem;
+  text-align: center;
+
+  transition: all ease-in-out 0.5s;
+
+  &:hover:not([disabled]) {
+    background: ${({ theme }) =>
+      backgroundHexColorLighten(
+        theme.colors.primary.main,
+        theme.colors.white.main
+      )};
+  }
+
+  &[disabled] {
+    cursor: auto;
+
+    ${({ theme, active }) =>
+      active
+        ? ButtonActiveAnimation
+        : `color: ${theme.colors.black.main + "30"};
+          box-shadow: ${boxShadowBorder(theme.colors.black.main + "26")};
+      `}
+  }
+`;
