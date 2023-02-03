b2d247858a9efe52bf2ae77099f701c78a4753a8
Tinywugaga <tiny98712@gmail.com>
2023-02-03 15:14:41 +0800

    add env

--- env.config.js
+++ env.config.js
@@ -3,7 +3,16 @@ const prod = process.env.NODE_ENV === "production";
 const localhost = "http://localhost:3000/";
 const apiDomain = prod ? process.env.DEPLOY_DOMAIN : localhost;
 
+const IS_DEV =
+  process.env.NODE_ENV === "development" ||
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "development";
+const IS_PREVIEW = process.env.NEXT_PUBLIC_VERCEL_ENV === "preview";
+const IS_PROD = prod && !IS_PREVIEW;
+
 module.exports = {
+  IS_DEV,
+  IS_PREVIEW,
+  IS_PROD,
   LOCAL_DOMAIN: localhost,
   API_DOMAIN: apiDomain,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY,
