1021f4ac4c3a7b1d08dc5bc5688a33ce866cc1b8
Tinywugaga <tiny98712@gmail.com>
2023-03-08 23:01:59 +0800

    fix wrong import variable

--- src/lib/tracker.ts
+++ src/lib/tracker.ts
@@ -1,4 +1,4 @@
-import logger from "./logger";
+import { logger } from "./logger";
 import { generateErrorEventParams } from "@/utils/generateEventParams";
 
 import { ConversationStatus } from "@/types";
