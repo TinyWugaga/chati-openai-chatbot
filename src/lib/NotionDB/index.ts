32ec5a680fac7540361a4c6064fb13ba257e39df
Tinywugaga <tiny98712@gmail.com>
2023-03-19 13:33:49 +0800

    handle NotionDB errors

--- src/lib/NotionDB/index.ts
+++ src/lib/NotionDB/index.ts
@@ -23,23 +23,31 @@ class NotionDB {
 
   async queryData(filter: QueryDatabaseParameters["filter"]) {
     if (this._notionDB) {
-      return await this._notionDB.databases.query({
-        database_id: this._databaseId,
-        filter,
-      });
+      try {
+        return await this._notionDB.databases.query({
+          database_id: this._databaseId,
+          filter,
+        });
+      } catch (error) {
+        console.error({ notionQueryDataError: error });
+      }
     }
   }
 
   /** ts issue when using CreatePageParameters["properties"] as the type of param properties */
   async addData(properties: { [key: string]: any }) {
     if (this._notionDB) {
-      return await this._notionDB.pages.create({
-        parent: {
-          type: "database_id",
-          database_id: this._databaseId,
-        },
-        properties,
-      });
+      try {
+        return await this._notionDB.pages.create({
+          parent: {
+            type: "database_id",
+            database_id: this._databaseId,
+          },
+          properties,
+        });
+      } catch (error) {
+        console.error({ notionAddDataError: error });
+      }
     }
   }
 
@@ -48,10 +56,14 @@ class NotionDB {
     properties: UpdatePageParameters["properties"]
   ) {
     if (this._notionDB) {
-      return await this._notionDB.pages.update({
-        page_id: pageId,
-        properties,
-      });
+      try {
+        return await this._notionDB.pages.update({
+          page_id: pageId,
+          properties,
+        });
+      } catch (error) {
+        console.error({ notionUpdateDataError: error });
+      }
     }
   }
 }
